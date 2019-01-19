eaf260a7aa4ebc536670c4dd34bdcca000c5648a
Sipos Levente <45292341+SiposLevente@users.noreply.github.com>
2019-01-19 13:11:20 +0000

    Updated partitioning
    Other storage devices can now remain plugged into the computer

---
+++ ArchInstall2.sh
@@ -0,0 +1,119 @@
+default_values(){
+	local valt
+	local def
+	local masik
+	
+	valt=$1
+	def=$2
+	masik=$3
+
+	if [ "$valt" != "$def" ]; then
+		if [ "$valt" != "$masik" ];then		
+			valt=$def
+		fi
+	fi
+	echo "$valt"
+}
+
+echo 'Setting timezone to Europe/Budapest...'
+ln -sf /usr/share/zoneinfo/Europe/Budapest /etc/localtime
+
+echo 'Generateing "/etc/adjtime"'
+hwclock --systohc
+
+echo 'Press ENTER to continue...'
+read
+clear
+
+echo 'Copying locales...'
+echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
+echo 'hu_HU.UTF-8 UTF-8' >> /etc/locale.gen
+echo 'Generateing locales...'
+locale-gen
+echo 'Press ENTER to continue...'
+read
+clear
+
+echo 'Setting system language to US english'
+touch /etc/locale.conf
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+echo 'Creating a keyboard layout...'
+declare KEYMAP
+
+echo 'What layout should the keyboard have? [hu, us] (default: hu)'
+read KEYMAP
+
+KEYMAP=$(default_values "$KEYMAP" "hu" "us")
+
+touch /etc/vconsole.conf
+echo "KEYMAP=$KEYMAP" >> /etc/vconsole.conf
+echo 'Press ENTER to continue...'
+read
+clear
+
+declare HOSTNAME
+echo 'Enter the name of the computer: '
+read HOSTNAME
+touch /etc/hostname
+echo "$HOSTNAME" >> /etc/hostname
+echo 'Press ENTER to continue...'
+read
+clear
+
+echo 'Creating host config'
+
+declare ipv4lh
+declare ipv6lh
+declare ipv4ld
+
+echo 'What is the IPv4 localhost'
+read ipv4lh
+echo "$ipv4lh	localhost" >> /etc/hosts
+
+echo 'What is the IPv6 localhost'
+read ipv6lh
+echo "$ipv6lh	localhost" >> /etc/hosts
+
+echo 'What is the IPv4 localdomain'
+read ipv4ld
+echo "$ipv4ld	$HOSTNAME.localdomain $HOSTNAME" >> /etc/hosts
+
+echo 'Press ENTER to continue...'
+read
+clear
+
+echo 'Creating new initramfs...'
+mkinitcpio -p linux
+
+echo 'Press ENTER to continue...'
+read
+clear
+
+echo 'Setting root password...'
+passwd
+
+echo 'Press ENTER to continue...'
+read
+clear
+
+declare GRUB
+echo 'Should GRUB be installed on the system? [y, n] (default: y)'
+read GRUB
+
+GRUB=$(default_values "$GRUB" "y" "n")
+
+if [ $GRUB == "y" ];
+then
+		echo 'Downloading and Installing GRUB and efibootmgr'
+		pacman -S grub
+		pacman -S efibootmgr
+
+		grub-install --target=x86_64-efi --efi-directory=/boot
+		grub-mkconfig -o /boot/grub/grub.cfg
+fi
+
+echo 'Exiting the mounted system'
+exit
+
+#Made by Sipos Levente (KeTl3r)
